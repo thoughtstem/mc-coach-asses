ed2288f7ec96504ce670399b71f736fbcbbb6b35
Sara <sara@thoughtstem.com>
2019-11-14 10:45:08 -0800

    fixed reprovide reference in info

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "mc-coach-assess")
 (define deps '("base" 
                "https://github.com/thoughtstem/flash-card.git"
-               "reprovide"
+               "reprovide-lang-lib"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mc-coach-assess.scrbl" ())))
