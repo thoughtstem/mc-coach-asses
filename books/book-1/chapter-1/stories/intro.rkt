3b954d3d6698d2f272114469671f6c150cb8510e
srfoster <stephen@thoughtstem.com>
2019-11-19 08:22:06 -0800

    ch1 intro

--- books/book-1/chapter-1/stories/intro.rkt
+++ books/book-1/chapter-1/stories/intro.rkt
@@ -15,6 +15,10 @@
   (Model city...)
   (What is a model?)
 
-  Here's the schedule for the day...
+  We'll taking you to several MetaCoders classrooms across the city.
+  We'll look at the things that are common about all MetaCoders coaches -- the things they all do.  The things we expect you to learn.
 
+  Most of all, we hope this chapter communicates to you the scope of the MetaCoders vision.  We need you to understand it because we need your help bringing that vision to life.  Thank you for being here.  We couldn't do this without you.  Together we can make this world a better place.
+
+  Welcome to Metapolis.
 }
