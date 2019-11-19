69f0e953138e2192fd28ec8dfa9b8bcd67bc4bd1
Stephen <stephen@thoughtstem.com>
2019-11-18 16:57:51 -0800

    some content

--- books/book-1/chapter-1/stories/class-snapshot.rkt
+++ books/book-1/chapter-1/stories/class-snapshot.rkt
@@ -58,7 +58,34 @@
 
        "We don't say lesson plan, and we don't say teach.  Again, you'll learn more about why we have our own words in Chapter ___.  The short answer is that coaching isn't quite the same as teaching, and classmaps aren't quite the same as lesson plans.  If it helps to think about them that way for now, that's fine.  I know I did when I first started."
 
+       "Okay, so let me see if I understand.  A MetaCoders class is a sequence of alternating Story Modes and Game Modes, and the map tells you which one to do when?"
 
+       "Daaaang," said Joseph.  "Are you sure you haven't already gone through the MetaCoders training?  Alright, since you're practically an expert, let me give you the official guidelines for Story Mode and Game Mode."  He rummages around in his pocket and finds a slip of paper that he hands to you.  It reads:
+
+       @card{
+        @b{Story Mode:}
+        @ul{
+          @li{one point of focus (coach, student)}
+          @li{can look a lot of different ways}
+          @li{lower risk/challenge for most students}
+          @li{lower activity level}
+        }
+
+        @b{Game Mode}
+        @ul{
+          @li{goal-oriented}
+          @{different students/srudent groups can have different goals}
+          @{multiple points of focus}
+          @{usually where coding takes place}
+          @{can be higher challenge/risk for students}
+        }
+       }
+
+      "Can I keep this?" you ask.
+
+      "Sorry," says Joseph.  "I need it back.  But the nice thing about us both being characters in a story is that we're not going anywhere.  Metapolis is timeless.  So is Babbage University.  So is this conversation we're having.  I'll always be right here, in Chapter 1, at Babbage University, teaching this exact class every time you come back.  So as long as you remember where you learned something in Metapolis, you can always find it again."
+
+ <<Edit bullets above.>>
        
 Outline:
 
@@ -72,18 +99,6 @@ Use Class Launch example map
 
 Coach is great an guiding students through the 2 mode of the Class Map model...
 
-Intro Story mode
- - one point of focus (coach, student)
- - can look a lot of different ways
- - lower risk/challenge for most students
- - lower activity level
-
- Intro Game Mode
- - goal-oriented
- - different students/srudent groups can have different goals
- - multiple points of focus
- - usually where coding takes place
- - can be higher challenge/risk for students
 
  Similarities in modes?
  - coach always engaged, managing space
