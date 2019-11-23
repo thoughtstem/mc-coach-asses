9878640ca436980250b2d000f8cf5580ac4362e2
Sara <sara@thoughtstem.com>
2019-11-22 16:53:04 -0800

    still more ch 3 edits

--- books/book-1/chapter-3/stories/about-story.rkt
+++ books/book-1/chapter-3/stories/about-story.rkt
@@ -95,115 +95,95 @@
   
   "Fortnite, DrRacket, Github.  And... I like YouTube."
 
-  So it goes as the rest of the students filter in.  Bruce restarts the game whenever new students arrive.  Occasionally, he switches topics.  For example, after a few rounds of "things you like about coding and computers", he does the "name game" where everyone takes turns saying their names and remembering the names of the people who went earlier.  Then he chooses the topic to "things that have computers inside of them" -- like air planes, thermostats, cell phones.  It's all the same game -- a memory game -- but the change of topic keeps it interesting both for the students and for you over the next 10 minutes.
-
-  @heading{2:05pm}
-
-  When class time finally arrives, Bruces asks everyone to sit down.  "Okay!  What we were just doing was Game Mode.  But now, we're going to switch to Story Mode.  But first, I want to ask, has anyone here taken a MetaCoders class before?"
-
-  One student named Dylan raises his hand.  
-
-  "Cool!  Who was your coach?"
-   
-  "Ummm... Coach Joseph," he says. 
-
-  "Can you tell everyone what the tradition is on the first day of a MetaCoders class?" Bruce asks.
-
-  "Umm... where you tell your story?" says Dylan.
-
-  "Right!" says Bruce.  "Every MetaCoders class, on the first day, the coach tells their own story about who they are and why they became a MetaCoders coach.  By the way, for those of you who might be forgetful.  My name is Bruce."  He says it like the shark in @i{Finding Nemo}.
-
-  @(img src: "http://4.bp.blogspot.com/-RBl5a9gHIl4/U74H69JVQbI/AAAAAAAABU8/84QIEWt2VNc/s640/tumblr_n0fj7pdSnv1shsvppo1_500.gif")
-
-  Everyone laughs.
-
-  "Thanks," says Bruce.  "I've been practicing that joke for weeks.  Anyway, here's my story."  He clears his throat.  
-"I am brand new to being a coach.  I bet some of you may have been coding longer than I have -- like Dylan.  Has anyone else done some coding before?"
-
-  A few of the students raise their hands.  "I coded in Scratch with my mom," says Ava. 
-
-  "Well that means you have been coders longer than I have!  But at MetaCoders, we don't just teach you how to code.  We teach you how to... Dylan, do you know?"
-
-  "Uhh, I forgot," says Dylan.
-
-  "That's okay," says Bruce.  "We teach you how to @i{learn} how to code --"
-
-  "Oh, yeah!" exclaims Dylan; then he covers his mouth when he realizes he has interrupted.
-
-  "At MetaCoders we code computers and we code our own brains.  Coding our own brains is what some people call 'learning'.  And even though I haven't been
-  coding computers as long as some of you have, I have been coding my own brain for a very, very long time.  
-  A lot of my work is with my hands -- I do carpentry, painting, fixing for people
-  around their house.  I had to learn what kind of wood to buy from the hardware store, how to cut it, what tools to use, how to use those tools.
-  All of that is stored in my brain now.
-  I made a @i{lot} of mistakes when I first started, and I had to learn how to learn from my mistakes.  I had to learn how to not
-  get frustrated with myself.  And I had to learn how to make myself stick with
-  something long enough to get really, really good at it.  It takes a lot of strength to keep learning something day after day, 
-  month after month, and year after year.  
-  But I learned how to become a strong person.  All of that is stored in my brain now.  And because of everything stored in my brain,
-  I can literally build an entire house with my hands."
-
-  Some of the kids murmer in awe.
-
-  "Yep.  Seriously. I built my mom a guest house in my own backyard.  It has a bottom floor and a loft.  
-  It even has running water, electricity, and air conditioning.  
-  I would never have been able to do that if I hadn't spent so many years learning.  I'm really good
-  at learning and I want to help you all learn how to learn too," Bruce says. 
-  "Anyway, I always thought coding was cool because I love to play video games with my kids -- 
-  like Fortnite and Minecraft.  I had always dreamed of making my own video game. 
-  When my friend Ruby told me about how she became a coding coach and learned
-  how to code with students like you, I decided to do it too.  Now, just in the last few months,
-  I've coded websites, I've coded animations, and I've coded games.  
-  I'm going to coach you on how to code -- and I'm going to coach you on how to learn.
-  That's why I'm here.  And that's my story."
-
-  Bruce makes a very theatrical bow.  The students giggle.  Dylan claps, which causes the rest to join in applause.
+  The game continues.  A few minutes later, Bruce moves them inside to the rug and continues to play inside. He restarts the game whenever new students arrive.  Occasionally, he switches topics;  for example, after a few rounds of "things you like about coding and computers", he does the "name game" where everyone takes turns saying their names and remembering the names of the people who went earlier.  Then he chooses the topic to "things that have computers inside of them" -- like air planes, thermostats, cell phones.  It's all the same game -- a memory game -- but the change of topic keeps it interesting both for the students and for you over the next 10 minutes while Bruce ducks in and out of the game, occasionally checking his phone and poking his head out the door as he collects students.
 
   @(hr)
 
-  "That was Story Mode," says Bruce.  "In MetaCoders classes, we have two modes that we switch 
-  between -- Game Mode and Story Mode."  He references the screen of a laptop he has set aside for himself.
-  You catch a glimpse of what you assume is a classmap.  You see a section labeled "Coach Story", followed by a 
-  section labeled "Code of Awesomeness Stories".  It is comforting to know that coaches do not
-  have to have the classmaps memorized.  The students certainly don't seem to care. 
+  @heading{2:05pm}
 
-  Bruce clears his throat. "Our next mode is 
-  another Story Mode -- but this time, I'm going to read you a story and it's going to seem like it
-  has nothing to do with coding... but it does.  It has a hidden message that I will ask you to
-  tell me afterwards.  Dylan, I will let you give people hints."
+   When the final student arrives, Bruce pauses the game and gets everyone's attention on him. "Okay!  What we were just doing was Game Mode.  But now, we're going to switch to Story Mode.  But first, I want to ask, has anyone here taken a MetaCoders class before?"
 
-  "Here's story number one.  Once, long ago, Woodpecker and Ostrich were building homes.  
-  They were friends, but each one was trying to build a bigger house than the other.
-  They got branches from the forest, and by the end of the first day, their houses where
-  both this big." Bruce holds both hands at knee level. 
-  "The next day, they got more branches from the forest, and kept building,
-  but both of them noticed that there were one or two termites in their houses.  Ostrich
-  @i{really} wanted to have the biggest house, so he ignored the termites and kept building,
-  so by the end of the day his house was this big and had two termites in it."  Bruce put his left hand at chest level. 
-  "But Woodpecker decided to spend only half of the day building his house, and the other half
-  pecking out the termites.  So by the end of the day, his house was only this big, but was termite-free."
-  Bruce puts his right hand at belt level.
+  Three students, Aurelia, Colin and Bai, raise their hands.  
 
-  "So it went, day by day.  Ostrich's house got bigger, but had more and more bugs." He raises his left hand to head level.
-  "Ostrich's house go bigger too, but more slowly, but it had zero termites." He raises his right hand to chest level.
+  "Cool!  Who were your coaches? Aurelia?"
+   
+  "Ummm... Coach Joseph," she says.
 
-  "One day, Ostrich's house was gigantic."  He raises his left hand as high as it can go.  "And Woodpecker's
-  house was medium sized." He raises his right hand to head level.  "The problem was that Ostrich's house now had
-  hundreds of bugs in it.  The house was huge, but it shook every time the wind blew.  The door kept falling off.
-  There were holes in the walls.  The kitchen had collapsed several times already.  
-  Ostrich found that he was spending less and less time making his house bigger
-  and more and more time rebuilding the parts that had been eaten away during the night. 
-  Woodpecker's house, on the other hand, was still steadily growing.
-  By the time winter had arrived, both houses were gigantic."  He puts both hands as high as they can go.
+  "Yeah, it was Coach Joseph last time," Bai says.  "But the time before that it was Coach Ruby. She was also our coach--"
 
-  "But when the very first winter storm came..." He uses his left hand to simulate a house teetering dramatically in the wind,
-  and... after a tense moment... ultimately collapsing.  "Ostrich's entire house came crashing down!"
+  "That's really cool, Bai, but can you raise your hand next time?" Bruce asks. Bai nods.
 
-  "Luckily, Woodpecker came out of her house to see what had happened and said, 'It's okay. My house is big enough for both of us.' 
-  So Ostrich spent the winter in Woodpecker's home.  Ostrich promised himself that he would be a better bug hunter 
-  when the next spring came.  And that," says Bruce, "is how the 'Be a Bug Hunter' story ends."
+  "Thank you, Bai. Colin, can you tell everyone what the first thing we do on the first day of a MetaCoders class?" Bruce asks.
 
-  He then asks various students to explain the moral in their own words, to link the moral to things they've
-  done before.  He asks them to explain in their own words what a computer bug is.  Dylan has a lot to say -- because
+  "The coach tells a story?" says Bai.
+
+  "Right, we tell @i{our own} story. A story about us." says Bruce.  "Every MetaCoders class, on the first day, the coach tells a story about themselves -- when and why they became a Coding Coach, a story about their own experience as a coder, or something like that. Ready to learn a little more about me?" Bruce asks. The students nod, a few say "yeah!"
+
+ "Alright! Hello my name is Bruce."  He says it like the shark in @i{Finding Nemo}.
+
+  @(img src: "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/bruce-finding-nemo.gif")
+
+  Everyone laughs.
+
+  "Thanks," says Bruce.  "I've worked on that impression for a long time."
+  
+  "So, I am brand new to being a Coding Coach." Bruce continues.  "I bet some of you have been coding longer than I have -- like Bai and Aurelia.  Has anyone else done some coding before?"
+
+  A few of the students raise their hands.  "I coded in Scratch with my mom," says Efan, one of the new students. 
+
+  "Well that means you, too, have probably been a coder longer than I have!  But at MetaCoders, we don't just teach you how to code.  We teach you how to... Aurelia, do you know?"
+
+  Aurelia shakes her head.
+
+  "That's alright," says Bruce. "Bai? Colin?" 
+
+  "Uhh, I forgot," says Bai as Colin shrugs.
+
+  "It's okay! We teach you how to @i{learn} how to code --"
+
+  "Oh, yeah!" exclaims Bai; then he covers his mouth when he realizes he has interrupted.
+
+  "At MetaCoders we code computers and we code our own brains.  Coding our own brains is what we usually call 'learning.'  And even though I haven't been coding computers for very long, I have been coding my own brain for a very, very long time.  A lot of my work is with my hands -- I do carpentry, painting, fixing for people around their house.  I had to learn what kind of wood or paint is best for each project, what tools to use, how to use those tools.  All of that information is stored in my brain now.  I made a @i{lot} of mistakes when I first started, and I had to learn how to learn from my mistakes.  I had to learn how to not get frustrated with myself.  And I had to learn how to make myself stick with something long enough to get really, really good at it.  It takes a lot of strength to keep learning something day after day, month after month, and year after year.  But I have been learning that stuff for a long time. And because of everything stored in my brain, I can literally build an entire house with my hands."
+
+  Some of the kids murmur in awe.
+
+  "Yep.  Seriously. I built my mom a guest house in my own backyard.  It has a bottom floor and a loft.  It even has running water, electricity, and air conditioning.  I would never have been able to do that if I hadn't spent so many years learning.  With all that practice, I'm really good at learning and I want to help you all learn how to learn too," Bruce says.
+
+  
+  "Anyway, I always thought coding was cool because I love to play video games with my kids -- like Fortnite and Minecraft.  I had always dreamed of making my @i{own} video game.  When my friend Ruby told me about how she became a coding coach and learned how to code with students like you, I decided to do it too.  Now, just in the last month, I've worked with Github, a coders website for hosting code, coded websites, and games just like the ones we are going to make in class together.
+  
+  "I'm excited to code more with you all, as I coach you along your own journey to become a coder. And, along the way, I'm going to share what I know about learning, and help you become SUPER STUDENTS who can learn anything!"
+
+  Bruce strikes a dramatic superhero pose and the students giggle.  Bai claps, which causes the rest to join in applause.
+
+  "Thank you, thank you," Bruce says.  "But enough about me; I want to get to know you!  Let's flip into Game Mode to play a quick getting-to-know-you game..."
+
+  Bruce goes on to lead the game, but we are going to skip over this for now.  Since the focus of this chapter is @b{Story Mode}, we are just going to check those out.  Don't worry! We'll revisit the first day games later on, in Chapter 5.
+
+  @(hr)
+
+  @heading{2:20pm}
+
+  After a fairly quick, fun game, Bruce gathers the students back on the classroom rug.  As they settle down, you see Bruce pick up a laptop that was set off to the side.  You peak over his shoulder to see what you assume is a classmap.  You see a section labeled "Coach Story", followed by a section labeled "Name Game." Bruce clicks on the following section, labeled "Code of Awesomeness Stories".  It is comforting to know that coaches do not have to have the classmaps memorized.  
+
+  Bruce clears his throat. "Now we are back to Story Mode -- but this time, I'm going to read you a story and it's going to seem like it has nothing to do with coding... but it does.  It has a hidden message that I will ask you to tell me afterwards.  Bai, Aurelia and Colin: I know you have heard these stories before, so I am going to ask you to give us hints if we need them, okay?"
+
+  "Here's the first story:" Bruce begins.
+
+ "Once, long ago, Woodpecker and Ostrich were building their homes.  Now, Woodpecker and Ostrich were really good friends, but they @i{also} really wanted to build the biggest house in their neighborhood.  They got branches from the forest, and by the end of the first day, their houses were both this big." Bruce holds both hands at knee level.
+
+ 
+  "The next day, they got more branches from the forest, and kept building, but both of them noticed that there were one or two termites in their houses.  Ostrich @i{really} wanted to have the biggest house, so he ignored the termites and kept building, so by the end of the day his house was this big and had two termites in it."  Bruce put his left hand at chest level.  "But Woodpecker decided to spend only half of the day building his house, and the other half pecking out the termites.  So by the end of the day, his house was only this big, but was termite-free." Bruce puts his right hand at belt level.
+
+  "So it went, day by day.  Ostrich's house got bigger, but had more and more bugs." He raises his left hand to head level. "Woodpeckers's house got bigger too, but more slowly. But it had zero termites." He raises his right hand to chest level.
+
+  "One day, Ostrich's house was gigantic."  He raises his left hand as high as it can go.  "And Woodpecker's house was medium sized." He raises his right hand to head level.  "The problem was that Ostrich's house now had hundreds of bugs in it.  The house was huge, but it shook every time the wind blew.  The door kept falling off.  There were holes in the walls.  The kitchen had collapsed several times already.  Ostrich found that he was spending less and less time making his house bigger and more and more time rebuilding the parts that had been eaten away during the night.  Woodpecker's house, on the other hand, was still steadily growing.  By the time winter had arrived, both houses were gigantic."  He puts both hands as high as they can go.
+
+  "But when the very first winter storm came..." He uses his left hand to simulate a house teetering dramatically in the wind,  and... after a tense moment... ultimately collapsing.  "Ostrich's entire house came crashing down!"
+
+  "Luckily, Woodpecker came out of her house to see what had happened and said, 'It's okay. My house is big enough for both of us.'  So Ostrich spent the winter in Woodpecker's home.  Ostrich promised himself that he would be a better bug hunter when the next spring came.  And that," says Bruce, "is how the 'Be a Bug Hunter' story ends."
+
+  He then asks various students to explain the moral in their own words, to link the moral to things they've done before.  He asks them to explain in their own words what a computer bug is.  Bai has a lot to say -- because
   he has heard this story in his last class (about 10 weeks ago).  It's clear that he has forgotten a lot and that
   he has insights today that he could not have had the first time he heard the story.  He alone is able to 
   explain that "writing code is like building a house" and that "each line is like another piece of wood".
@@ -212,9 +192,9 @@
 
   You realize that the point of the stories is not about communicating a single moral -- like: "Fix bugs" --
   and more about engaging in a rich and textured discussion about how that story and its morals relates 
-  to the lives and worldviews of the students.  Seeing how much Dylan is benefitting from discussing
+  to the lives and worldviews of the students.  Seeing how much Bai is benefitting from discussing
   this story for the second time, and how much the other students are benefitting from his insights 
-  is inspiring.  You predict that in Dylan's next class, he may be able to tell at least one of these
+  is inspiring.  You predict that in Bai's next class, he may be able to tell at least one of these
   stories himself.
 
   Bruce goes on to tell two other fabels, each with slightly different morals.  Bruce references 
@@ -283,7 +263,7 @@
   a programming language for making adventure games.  Can anyone guess what that language is
   named?"
 
-  Ava and Dylan both raise their hands.
+  Ava and Bai both raise their hands.
 
   "Ava?"
 
@@ -355,7 +335,7 @@
   We always run the same classmap on the first day of any class.  It gives returning
   students a chance to establish themselves as leaders, and it gives new students a 
   chance to to get oriented.  It was my first time doing it.  You'll do it too one day.
-  Maybe Dylan, if he becomes a junior coach will do it."
+  Maybe Bai, if he becomes a junior coach will do it."
   
   "How much of it did you have memorized?" you ask.
 
