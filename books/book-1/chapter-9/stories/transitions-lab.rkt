3757bd600beba470a09266c3b6a4ca70f97278aa
Stephen <stephen@thoughtstem.com>
2019-11-28 11:21:00 -0800

    ch9 content

--- books/book-1/chapter-9/stories/transitions-lab.rkt
+++ books/book-1/chapter-9/stories/transitions-lab.rkt
@@ -8,26 +8,22 @@
                         (moment 2021 8 13)))
 (define links   (list))
 
-(require "../flash-cards.rkt")
+(require "../flash-cards.rkt" mc-classmaps/site/transitions)
 
 @paras{
-   Back at the Department of Neuroscience, <<you go to the "observation deck">>
+   Back at the Department of Neuroscience, you pass Professor Istoria's office and continue on to Dr. Joy Siegmund's lab.
 
-   Dr. Joy Siegmund <<meets you>> 
-
-   "Back for more?"
+   Dr. Joy motions you in with one hand as she configures a coffee maker with her other hand. "Back for more?"
 
    "Yes, but this time--" you begin.
 
-   "Here," she says, handing you a Nintendo Switch.  "Play." 
+   "Here," she says, handing you a Nintendo Switch that had been sitting next to the coffee maker.  "Play." 
 
-   You are about to protest, but you see Tetris on the screen.  Not one to let a game of Tetris be lost without reason, you begin to play...
-
-   She guides you to a chair.  You murmer a thanks as you continue.  The game gets faster and faster, but just as you are about to win, she returns and says, "I'm ready now.  I can take that off your hands."
+   You are about to protest, but you see Tetris on the screen.  You're not one to let a game of Tetris be lost without reason, so you begin to play.  She guides you to a chair.  You murmer a thanks as you continue.  The game gets faster and faster, but just as you are about to win, she returns and says, "I'm ready now.  I can take that off your hands."
 
    "One moment," you say, "I'm almost --"
 
-   She knocks the Nintendo Switch out of your hands, just as you are about to sink one of the long skinny blocks into the perfect spot.  
+   Just as you are about to sink one of the long skinny blocks into the perfect spot, she knocks the Nintendo Switch out of your hands and onto the carpet.
 
    "Hey!" you protest.  
 
@@ -35,9 +31,120 @@
 
    "You could have warned me at least," you say.
 
-   "Exactly," she says.  "Flow states are enjoyable.  We don't like having them interrupted.  Being forwarned does help, though."
+   "Exactly," she says.  "Flow states are enjoyable.  If you're going to interrupt one, it's certainly polite to warn someone."
+
+   "So why didn't you do it?" you ask, picking up the Nintendo Switch and observing the heart-wrenching "Game Over" message superimposed on a messy tower of colored blocks.
+
+   "MetaCoders sends you to the Department of Neuroscience in hopes that you'll return with jewels of psychological insight that will transform how you interact with the students you coach."
+
+   "So: 'Don't be mean to people.'  Is that the insight?"
+
+   "What are the two classroom modalities in a MetaCoders classroom?"
+
+   "Game Mode and Story Mode," you recite without effort -- fluent recall.
+
+   "And if there are two modalities, then there are four possible transitions between modalities," she says.  "What are they?"
+
+   "I've seen coaches start in Story Mode and then switch to Game Mode.  And I've seen coaches move from Game Mode to Story Mode.  But if you're telling me there are four transitions, then I guess it's possible to switch from one Story Mode to another Story Mode, or from one Game Mode to another Game Mode."
  
-     
+   "I knocked the game out of your hands to give you first-hand experience with the hardest of those four transitions.  A moment ago you were playing a game, in a flow state -- now you're back in reality, having a boring conversation with me." 
+
+   "It's not boring..." you say.  "Actually, now that I think about it -- it's not necessarily less enjoyable than Tetris."  But you can still vividly recall how angry you were when she suddenly knocked the game out of your hands.  The sudden loss of control, the sudden inability to finish up the short-term goal of sinking that long skinny block into its appropriate place.  "It's like the classic story of a parent telling the kids that dinner is ready -- but the kids won't come because they're in the middle of a game.  And then if the parent goes up and unplugs the game, the kids are suddenly angry."
+
+   "And is it because they hate food?" Dr. Joy asks.  
+
+   "Doubtful," you say. 
+
+   "Is it because games are more important than food?"
+
+   "Certainly not," you say.
+
+   "What is it that the brain does during a flow state that makes stopping so unpleasant?"
+
+   "When you really get engrossed in something," you say, "you... I don't know... 'zoom in' and it becomes the most important thing in the world.  All of your attention focuses in on it."
+
+   "That's a textbook definition of flow state," she says.  "And it's from that intense focus that all of the positive learning properties of games come from.  Instead of being half-present as you learn something, you're fully engrossed.  Your entire working memory and attention are allocated to the task at hand.  Almost naturally, you become better at the task, more fluent, more able to take on harder challenges in the future.  But... it's a double-edged educational tool."
+
+   You hand the Nintendo Switch back to her.  "Because you can't play games for an entire class.  Are coaches doomed to perpetually frustrate our students?  Giving them games and then knocking them out of their hands?"
+
+   "Let's invent a game," she says.  "Let's call it: Interruption Tetris.  The rules are, first, I hand you a game of Tetris."  She hands you the Switch.  "Then, I tell you that I'm setting my timer for a random amount of time -- between five and ten minutes -- but I don't tell you the exact number.  Then, when the timer goes off, I snatch the Switch from your hands and ask you to a task -- like 100 jumping jacks, or read a short story out loud, or draw a picture of a cat.  Then, I'll give you the game back.  And we continue in that fashion for a hour or so.  You win the game if you manage to 1) not lose at Tetris, and 2) perform all of the interruption tasks successfully."
+
+   "I don't know if I have time to..."
+
+   "We're not really going to play Interruption Tetris," she says.  "It's a thought experiment.  My question to you is: @i{If} you practiced, would you be able to get good at Interruption Tetris?  @i{If} you were competing against others, do you think you could (with training) become better and better at it?"
+
+   "Sure," you say.  "I suppose that with practice, leaving the flow state might become less annoying -- or I might even remain in a state of flow as I do the interruption tasks.  Lots of games have smaller mini-games inside of them.  As long as a player knows that certain activities will get interrupted, they can get used to it, or even find the interruptions to be an enjoyable challenge."
+
+   "If you're going to use the power of flow states in your classrooms, you need to be skilled at transitioning into them and out of them," says Dr. Joy.  "And so do your students.  As you practice transitions, students will become better at it.  Furthermore, you can make the transitions smoother by announcing that they are coming up -- for example, 'Five minutes until I'm going to ask you to shut your computers.'  And there are two other things...  One is a trick from the science of stories, and the other is a trick from the science of games...  Do you know what they are?"  
+
+   "I have no idea," you say.
+
+   "The trick from the science of stories," she says, "is to make sure that positive and productive stories surround the classroom game playing.  If a basketball coach @i{tells} their students that the next drill is intended to sharpen their ability to work together in a team, then when the drill is over and student are disappointed, you can remind them of the bigger picture -- that they are there to become better basketball players, and they can't spend all of their time on just one kind of skill-building activity.  Human beings think in stories.  Your students will think in stories too -- both their own and the ones you help them construct.  Picking the right stories to explain why you are playing a game and why you are stopping -- that's the art of coach storytelling right there.  Students know that a coach's job isn't to create an environment of fun and games and uninterrupted flow states.  Everyone knows that a coach's job is also to help people grow."
+
+   "What's the trick from the science of games?" you ask.
+
+   "Environment alterations," she says.  "Video games are great at conveying a sense of place.  In games like Skyrim, you move from house to house, and from city to city.  Even in simple games like Tetris, when you progress to a new level, the background changes, giving the illusion that you are playing in a new location, even though you haven't moved an inch in real life.  Human brains organize information spatially.  Have you ever walked into a room and forgotten why you came?"
+
+   "Everybody does.  What's the trick, though?  Change the classroom environment?"
+
+   "Let me tell the story first," she says.  "Thousands of years ago, the ancient Greeks invented a memory trick that is still used today by elite memory atheletes.  Do you know about elete memory atheletes?"
+
+   "Like people who compete to memorize decks of cards and digits of pi?"
+
+   "Exactly," she says.  "It's called the @(link-to "https://en.wikipedia.org/wiki/Method_of_loci" "'method of loci'").  You simply take the things you want to memorize, and you visualize yourself walking through a place you know.  You would visualize yourself placing the objects you want to remember in different parts of that place.  It could be rooms in your house.  It could even be locations in the city of Metapolis.  It doesn't matter -- as long as you can visualize it.  Anyway, there are thousands of years worth of anecdotal evidence and a mountain of modern science to suggest that this technique increases the user's memory capacity, and every elite memory athelete today uses one or more variations on the method of loci." 
+
+   "That's cool," you say, "but I'm getting lost.  What's the trick?"
+
+   "The trick is to put Story Mode and Game Mode into different environments," she says.  "Exactly how you do this will depend on how the classroom is set up.  Come with me." She takes you over to the part of her lab that is set up like a mock classroom.  The two of you stand together at the front of the space, where the whiteboard is.  "For example, whenever you tell a story, maybe you ask all the students to get up from their desks and join you over here."  She walks to the part of the mock classroom where a circle of beanbag chairs are.  
+
+   "And if the classroom doesn't have a storytelling space?" you say.
+
+   She walks back to the front of the classroom.  "Remember that Tetris simply swaps its background on every level."  She searches for something on her smartphone, then shows you an image:
+
+   @(img src: "https://en.wikipedia.org/wiki/File:Tetris_screenshot_(Amiga,_Infogrames).png")
+
+   "That's from a 1988 version of Tetris, which swapped to various Russia-related images on every level -- like this one of Yuri Gagarin.  It's just an image swap -- but one minute, you're playing in front of the Kremlin, and the next, you're in a space shuttle with Gagarin.  The take-away message is that superficial changes in the environment can be just as effective as actually relocating.  For example, you could dim the lights when you begin a Story Mode.  You can (and definitely should) tell the students to close their laptops.  Anything you do to phsyically and visually distinguish the Story Mode environments from your Game Mode environments will help students understand what modality you want them to be in.  Change the space, change the mind."
+
+   "And I suspect that if you keep using the same environmental cues over and over, the class will get better and better and switching their brains into different modes."
+
+   "Exactly.  Classes get better and better and transitioning together and at understanding when and why they are transitioning.  No matter how skilled a coach you are on your first day of a class, everything will always be smoother on your last day of class @i{if}..." She trails off, looking at you to complete the sentence.
+
+   "If we practice as a class.  If I teach the students what Game Mode and Story Mode are.  If I help them understand how to transition.  If we practice transitioning.  Could I get a reminder card for all of this?"
+
+   She rummages around in her pocket and comes back with the following card.  
+
+   @card{
+     @card-body{
+       @card-text{
+         @(game-to-story)
+       }
+     }
+   }
+
+   "What about the other transitions?  Story Mode to Game Mode, or Story Mode to Story Mode?"
+
+   "Remember that the Game Mode to Story Mode transition is the hardest one.  It's the one where you have to battle the natural human tendency to want to preserve our flow states from interruption.  It's the transition where you'll be most likely to repeat yourself -- for example, 'I said @i{close the computers!}  It's the one you'll have to practice the most -- and the one you'll have to help your students practice the most."
+
+   "Still," you say, "I'd like to see a card for the other transitions if you have one."
+
+   "Here's one for the Story Mode to Game Mode transition," she says.  "For the other ones, I'll refer you to the materials in the Appendix Chapter of this training book.  MetaCoders gives you a ton of information about classmaps, Game Modes, Story Modes, transitions, and attention getters.  I'm not here to teach you all of that.  I'm just here to teach you the scientific underpinnings of it all."  Finally, she hands you the card.
+   
+   @card{
+     @card-body{
+       @card-text{
+         @(story-to-game)
+       }
+     }
+   }
+
+   "I see," you say.  "They're like the reverse of each other.  To start a Game Mode, you just use the fact that you already have everyone's attention, and you tell them how the game works, get them excited, and change the space.  To go back to Story Mode, you change the space at the beginning, and then slowly transition them back to the Story Mode listening state, and start the story."
+
+   "I can't take credit for designing it," says Dr. Joy.  "I'm just here to tell you the science behind the details."
+ 
+   You finish your last sip of coffee and hand it back to Dr. Joy, half expecting her to knock it out of your hand in the interest of teaching another lesson.  But she simply takes it.  As you leave the building, you realize that this is the last chapter of this training book.  You wonder if you'll ever be back to Babbage University.
+
+   As you pass Babbage's Analytical Engine on the way out, you stop to admire the mechanical complexities of the machine.  You find yourself thinking about classrooms as machines too -- built not from gears and rods, but from human brains and bodies.  The Analytical Engine has its modes and transitions too, no doubt; but it's impossible to tell what they are simply by looking at the contraption.  But you suspect that no matter how complex Babbage's machine is, it can't compare to the complexities of an 'educational machine' constructed out of a coach, a bunch of computers, and coding students.  The punchcards hanging in a plastic baggy on the wall remind you of classmaps -- programs, but for a more predictable machine.
+  
 }
 
 
