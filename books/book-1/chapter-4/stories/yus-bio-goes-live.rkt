90d5020c2699f906e89e582596ac4b596fc97c73
Sara <sara@thoughtstem.com>
2019-12-04 14:05:26 -0800

    complete edits for ch4

--- books/book-1/chapter-4/stories/yus-bio-goes-live.rkt
+++ books/book-1/chapter-4/stories/yus-bio-goes-live.rkt
@@ -1,18 +1,23 @@
 #lang at-exp metapolis-stories/lang/story
   
-(define title         "Yu's Bio Goes Live")
+(define title         "Yu's Bio")
 (define place         places:coder-cafe)
 (define characters    (list characters:yu))
 (define time          (unnamed-time
-                        (moment 2021 6 28)
-                        (moment 2021 8 13)))
+                        (moment 2021 1 5 13)
+                        (moment 2021 1 5 13 30)))
 (define links   (list))
 
 (require "../flash-cards.rkt"
          mc-coach/bio)
 
 @paras{
-  Yu is sitting in the same place as last time, with the same coffee mugs.  "Are you ready?" Yu asks when you sit down.  "I'm still working on my bio, but check it out."
+
+       You step into the coffee shop, relieved to be out of the cold.  Once again, none of the patrons of the shop look up, all of them too engrossed in the work on their computer screens.  It's subtly disorenting.
+       
+  Yu is sitting in the same place as last time, with the same coffee mugs.  You join him at his table.
+
+ "Are you ready to build your coach bio?" Yu asks when you sit down.  "I'm still working on mine, but check it out."
 
   Yu first shows you the code in DrRacket:
 
@@ -21,20 +26,19 @@
       #lang mc-coach/bio
 
       (bio
-        #:name  "Yu" 
+        #:first-name  "Yu" 
         #:profile-pic (square 40 'solid 'red)
-        #:coach-story "A few months ago, I began training as a MetaCoders coach.  Here's my story..."
-      )
+        #:coach-story "A few months ago, I began training as a MetaCoders coach...")
     }
   }
 
   Then Yu shows you the webpage that the code produces when it runs:
 
   @(bio
-    #:name  "Yu" 
+    #:first-name  "Yu" 
     #:profile-pic (square 40 'solid 'red)
-    #:coach-story "I've just begun training as a MetaCoders coach.  Here's my story...")
+    #:coach-story "I've just begun training as a MetaCoders coach...")
   
-  "You're welcome to join me.  I'm going to keep working on it," says Yu.  "I haven't even added my actual profile picture."
+  "You're welcome to join me.  I'm going to keep working on it," says Yu.  "I haven't even added my actual profile picture..."
 
 }
