c664f4fce9112bcd1e04680501556763188d01f6
Sara <sara@thoughtstem.com>
2020-01-31 16:56:32 -0800

    fixing my error ugh friday brains are the worst
    This reverts commit b140245654af421382c2c9861bcaa4aa2cdd6708.

--- info.rkt
+++ info.rkt
@@ -2,6 +2,16 @@
 (define collection "mc-coach-assess")
 (define deps '("base" 
                "https://github.com/thoughtstem/flash-card.git"
+               "https://github.com/thoughtstem/website.git"
+               "https://github.com/thoughtstem/define-assets-from.git"
+               "https://github.com/thoughtstem/mc-classmaps.git"
+               "https://github.com/thoughtstem/mc-languages.git"
+               "https://github.com/thoughtstem/mc-coach.git"
+               "https://github.com/thoughtstem/mc-coach-assess.git"
+               "https://github.com/thoughtstem/metapolis-stories.git"
+               "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"
+               "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
+               "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-survival"
                "reprovide-lang-lib"))
 (define compile-omit-paths '("cards/"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
